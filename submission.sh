37ccbeef2998fb40023c93d58859b5dc80f8253d
Matt Durrant <durrantmm@gmail.com>
2017-06-22 16:48:24 -0600

    changed cluster threading

--- submission.sh
+++ submission.sh
@@ -26,4 +26,4 @@
 #
 #$ -cwd
 
-snakemake --cluster-config cluster.json --cluster "qsub -V -l h_vmem={cluster.mem} -l h_rt={cluster.time} -l s_rt={cluster.time} -A {cluster.account}" -j 200 -p
+snakemake --cluster-config cluster.json --cluster "qsub -V -l h_vmem={cluster.mem} -l h_rt={cluster.time} -l s_rt={cluster.time} -A {cluster.account} -pe threaded {cluster.threads}" -j 200 -p
