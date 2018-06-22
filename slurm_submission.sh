b540189821e7c00ec5ca90258e413224e06c6d84
Matt Durrant <durrantmm@gmail.com>
2018-06-21 18:19:34 -0600

    slurm_sbmission_script

--- slurm_submission.sh
+++ slurm_submission.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+#
+#SBATCH --job-name=snakemake_slurm_submission
+#SBATCH --output=snakemake_slurm_submission
+#
+#SBATCH --ntasks=1
+#SBATCH --time=168:00:00
+#SBATCH --mem=1G
 
 snakemake --use-conda --cluster-config slurm_cluster.json --cluster "sbatch -A {cluster.account} -p {cluster.partition} -n {cluster.n}  -t {cluster.time} --mem {cluster.mem}" -j 200 -p
